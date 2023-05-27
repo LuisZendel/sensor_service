5c2efb6c3b648fd3ae6e9628d06e5e7359e548c9
Luis Zendel Samperio <serverless.19@gmail.com>
2023-05-27 14:44:46 -0600

    feat: add post

--- config/mysql.js
+++ config/mysql.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-const  con = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
   user: "robot",
   password: "1234",
   database: "DIPUTADOS",
 });
 
-const gatDataSensor = ({req,res}) => {
+const gatDataSensor = ({ req, res }) => {
   con.query("select * from datasensor", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
@@ -14,14 +14,16 @@ const gatDataSensor = ({req,res}) => {
   });
 };
 
-const postDataSensor = ({req,res}) => {
-    console.log(req)  
-  /*const querySql = "insert into datasensor (valueSensing) values ("+ data.value +");";
+const postDataSensor = ({ req, res }) => {
+  console.log(req.body);
+  const data = req.body;
+  const querySql =
+    "insert into datasensor (valueSensing) values (" + data.value + ");";
   con.query(querySql, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.json(result);
-  });*/
-}
+  });
+};
 
-module.exports = {gatDataSensor, postDataSensor}
+module.exports = { gatDataSensor, postDataSensor };
